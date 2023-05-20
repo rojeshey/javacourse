947259501cbb1a489e6d287655ec40c8969ed1f0
rojeshey <95028491+rojeshey@users.noreply.github.com>
2023-05-20 22:55:51 +0545

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,68 @@
+/*
+
+
+// value and variable
+
+
+let country = "nepal";
+let continent = "asia";
+let population = "10000000";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+// data types
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
+
+//  let ,const and var 
+
+let langauage = "nepali";
+console.log(langauage);
+
+let price = 3000;
+console.log(price);
+price = 3500;
+console.log(price);
+
+price = 4000;
+console.log(price);
+
+
+
+
+
+
+
+// basic operators
+
+let population = 100000;
+console.log(population / 2);
+
+population++;
+
+
+let finland = 6000000;
+console.log(finland >= population);
+
+console.log(population < 33000000);
+
+let description = "portugal is in europe and its 11 million people speak portuguese";
+console.log(description);
+
+
+*/
+
+
+
+
---
+++ script.js
@@ -0,0 +1,178 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23);
+
+let firstname = "matila";
+
+
+
+
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+// variable name conventions
+
+
+let jonas_matila = "jm";
+let $function = 27;
+
+
+let person = "jonas";
+
+let PI = 3.1415;
+
+let myFirstJob = "coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "jonas");
+
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = "programmar";
+job = "teacher";
+
+lastName = "rojeshneupane";
+console.log(lastName);
+
+
+
+// math opreators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2** 3 means 2 to the power of 3= 2* 2* 2
+
+
+const firstName = "jonas";
+const secondName = "schmedmann";
+
+console.log(firstName + " " + secondName);
+
+
+//assignment operators
+let x = 10 + 5; //15
+x += 10; // x=x+10 = 25 
+x *= 4; // x=x*4=100
+x++;// x=x+1
+x--;// x=x-1
+x--;
+console.log(x);
+
+
+//comparison operators
+
+console.log(ageJonas > ageSarah);// >,<,>=,<=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+
+
+
+let x, y;
+x = y = 25 - 10 - 5;// x=y=10 ,x=10
+console.log(x, y);
+
+const averageAge =(ageJonas + ageSarah )/ 2
+console.log(ageJonas, ageSarah, averageAge);
+
+
+
+
+
+
+// *** bmi coding challenges
+
+
+//data 1
+
+// const markWeight = 78;
+// const markHeight = 1.69;
+// const johnWeight = 92;
+// const johnHeight = 1.95;
+
+
+//data 2
+const markWeight = 95;
+const markHeight = 1.88;
+const johnWeight = 85;
+const johnHeight = 1.76;
+const bmiOfMark = markWeight / markHeight ** 2;
+
+const bmiOfJohn = johnWeight / johnHeight ** 2;
+console.log(bmiOfMark, bmiOfJohn);
+
+const markHigherBMI = bmiOfMark > bmiOfJohn;
+console.log(markHigherBMI);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
